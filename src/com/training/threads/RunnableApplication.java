e953cec06c84102139ad2e8831b55117b0ea6e20
randika97 <randikasuridu@gmail.com>
2022-02-17 01:07:41 +0530

    Thread and Runnable examples

--- src/com/training/threads/RunnableApplication.java
+++ src/com/training/threads/RunnableApplication.java
@@ -5,6 +5,9 @@ public class RunnableApplication {
 
         Runnable runnable = new Runnable();
         Thread thread=new Thread(runnable);
+        thread.setPriority(10);
+        System.out.println("Main thread Priority"+Thread.currentThread().getPriority());
+        System.out.println("Child thread Priority"+thread.getPriority());
         thread.start();
         for(int i=1;i<50;i++){
             System.out.println("Main thread loop value is : "+Thread.currentThread().getName()+""+i);
