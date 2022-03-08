72e3df425c495d7caa48ba6cef386a209461b6a5
randika97 <randikasuridu@gmail.com>
2022-03-08 16:50:03 +0530

    Singleton pattern with early and lazy Instantiation

---
+++ training/src/com/training/designPatterns/singletonPattern/Application.java
@@ -0,0 +1,25 @@
+package com.training.designPatterns.singletonPattern;
+
+public class Application {
+    public static void main(String[] args) {
+
+        //illegal construct.Compile Time Error: The constructor SingleObject() is not visible
+       /* SingleObject object = new SingleObject();*/
+        //Get the only object available with Early Instantiation
+        EarlyInstantiation object = EarlyInstantiation.getInstance();
+        System.out.println(object.hashCode());
+        //show the message
+        object.showMessage();
+
+        // Instantiating Singleton class with variable lazyInstantiation
+        LazyInstantiation lazyInstantiation = LazyInstantiation.getLazyInstantiation();
+        System.out.println(lazyInstantiation);
+        // Instantiating Singleton class with variable lazyInstantiation1
+        LazyInstantiation lazyInstantiation1 = LazyInstantiation.getLazyInstantiation();
+        System.out.println(lazyInstantiation1);
+
+        System.out.println("Hashcode of x is "+ lazyInstantiation.hashCode());
+        System.out.println("Hashcode of y is "+ lazyInstantiation1.hashCode());
+
+    }
+}
